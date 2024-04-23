b13c2ccc90c5165da09f7f6508f19ab1363dac72
Burak <burak.dag@powercoder.org>
2024-04-23 15:38:37 +0200

    Exercise switch-case

---
+++ Week_4/Translate days of the week/main.js
@@ -0,0 +1,27 @@
+
+let translateDay = prompt("Enter day's name:")
+
+switch(translateDay){
+  case "Monday":
+   alert("Lundi") 
+   break;
+  case "Tuesday":
+    alert("Mardi")
+    break;
+  case "Wednesday":
+    alert("Mercredi") 
+    break;
+  case "Thursday":
+    alert("Jeudi")
+    break;
+  case "Friday":
+    alert("Vendredi")
+    break;
+  case "Saturday":
+    alert("Samedi")
+    break;
+  case "Sunday":
+    alert("Dimanche")
+    default:
+        translateDay=alert("Error!!!")          
+}
