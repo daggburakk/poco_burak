56b3e2c728d19ef8ef0f9cf3844d7d26f78be189
Burak <burak.dag@powercoder.org>
2024-05-01 13:48:45 +0200

    Create HTML and JS file to find the biggest number

---
+++ Week_5/number_bigger/app.js
@@ -0,0 +1,8 @@
+let numberFirst=10;
+let numberSecond=5;
+
+if (numberFirst>numberSecond){
+    console.log("The greater number of"+ numberFirst + "and" + numberSecond + "is"+ numberFirst);
+}else{
+    console.log(`The smaller number of ${numberFirst} and ${numberSecond} is ${numberSecond}`);
+}
